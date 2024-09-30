35345d75bf44a14959a09bff8947c96b2ee30c80
NTLONG_VN <longnt2302@gmail.com>
2024-09-30 09:16:04 +0700

    first push

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Button } from "antd";
+
+function App() {
+  return <Button>abc</Button>;
+}
+
+export default App;
