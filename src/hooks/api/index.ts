047fe34438da5080fb72a20c31dd2e8cf8eb97a8
NTLONG_VN <longnt2302@gmail.com>
2024-10-15 23:15:06 +0700

    update

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -1,9 +1,3 @@
 export * from "./useSearchViTri";
 export * from "./useRegister";
 export * from "./useLogin";
-<<<<<<< HEAD
-export * from "./useAddViTri";
-export * from "./useUserEdit";
-export * from "./useBooking";
-=======
->>>>>>> devlong
